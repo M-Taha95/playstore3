4ca50b2db73266931cea6d6ec2181ffb06e1802b
M-Taha95echo <msoudi2013@gmail.com>
2024-01-10 11:32:05 +0300

    Caching by Redis

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,15 @@
+from django.core.cache import cache
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from rest_framework.views import APIView
 from .tasks import notify_customers
+import requests
 
 
-def say_hello(request):
-    notify_customers.delay("Hello")
-    return render(request, "hello.html", {"name": "Mohammed Taha"})
+class HelloView(APIView):
+    @method_decorator(cache_page(5 * 60))
+    def get(self, request):
+        response = requests.get("https://httpbin.org/delay/2")
+        data = response.json()
+        return render(request, "hello.html", {"name": "MOHAMMED"})
